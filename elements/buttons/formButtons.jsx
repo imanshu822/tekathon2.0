a1cbabfbdacde8df3614526137ed694c87d4058b
Anshu <imanshu822@gmail.com>
2023-09-21 11:01:00 +0530

    Delete elements/buttons directory

--- elements/buttons/formButtons.jsx
+++
@@ -1,7 +0,0 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-
-const FormButton = ({ icon, size }) => {
-  return <FontAwesome name={icon} size={size} />;
-};
-
-export default FormButton;
